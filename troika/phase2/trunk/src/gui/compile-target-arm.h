a65a37d56b3f83cf09f430595d41d4bdde90e561
(no author) <(no author)@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2006-02-07 00:57:53 +0000

    git-svn-id: http://svn.fortytwo.net/projects@83 4bf1a796-0a03-0410-9b82-c499c1e613c7

--- troika/phase2/trunk/src/gui/compile-target-arm.h
+++
@@ -1,6 +0,0 @@
-#ifndef compile_target_h
-#define compile_target_h
-
-#define ARM_COMPILE
-
-#endif
--- troika/phase2/trunk/src/gui/compile-target-x86.h
+++
@@ -1,6 +0,0 @@
-#ifndef compile_target_h
-#define compile_target_h
-
-#define X86_COMPILE
-
-#endif
