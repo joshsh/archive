8a3d52598814cfcb09e3161a243a804ba4ef9b91
(no author) <(no author)@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2006-02-11 02:39:38 +0000

    Added mini-scanners to p2_parser.l (needs more work).
    git-svn-id: http://svn.fortytwo.net/projects@87 4bf1a796-0a03-0410-9b82-c499c1e613c7

---
+++ troika/phase2/trunk/src/api/p2_environment.h
@@ -0,0 +1,71 @@
+#include "util/p2_dictionary.h"
+
+/** A data structure which is equivalent to a namespace containing uniquely
+    named data types, function primitives, and data sets. */
+typedef struct _p2_environment
+{
+    p2_dictionary *types;
+    p2_dictionary *primitives;
+    p2_dictionary *datasets;
+
+} p2_environment;
+
+/** \return  a new environment */
+p2_environment *p2_environment__new();
+
+/** Destroys an environment. */
+void p2_environment__delete( p2_environment *env );
+
+p2_type *p2_register_type( p2_environment *env, p2_type *type );
+p2_type *p2_resolve_type( p2_environment *env, char *name );
+p2_primitive *p2_register_primitive( p2_environment *env, p2_primitive *prim );
+p2_primitive *p2_resolve_primitive( p2_environment *env, char *name );
+//p2_dataset *p2_register_dataset( p2_environment *env, p2_dataset *dataset );
+//p2_dataset *p2_resolve_dataset( p2_environment *env, char *name );
+p2_atom *p2_resolve_atom( p2_environment *env, char *dataset_name, char *atom_name );
+
+
+--------------------------------------------------------------------------------
+
+
+#include "p2_environment.h"
+#include "p2_type.h"
+#include "p2_primitive.h"
+#include "p2_dataset.h"
+
+
+p2_environment *p2_environment__new()
+{
+    p2_environment *env
+        = ( p2_environment* ) malloc( sizeof( p2_environment ) );
+
+    // Create data types dictionary.
+    env->types = p2_dictionary__new();
+
+    // Create function primitives dictionary.
+    env->primitives = p2_dictionary__new();
+
+    // Create data sets dictionary.
+    env->datasets = p2_dictionary__new();
+
+    return env;
+}
+
+
+void p2_environment__delete( p2_environment *env )
+{
+    // Destroy data sets dictionary and all data sets.
+    p2_dictionary__for_all( env->datasets, p2_dataset__delete );
+    p2_dictionary__delete( env->datasets );
+
+    // Destroy function primitives dictionary and all function primitives.
+    p2_dictionary__for_all( env->primitives, p2_primitive__delete );
+    p2_dictionary__delete( env->primitives );
+
+    // Destroy data types dictionary and all data types.
+    p2_dictionary__for_all( env->types, p2_type__delete );
+    p2_dictionary__delete( env->types );
+
+    free( env );
+}
+
