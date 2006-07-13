aeafbe427155df0664f323c76eae5e07802c3149
joshs <joshs@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2006-07-13 19:24:46 +0000

    git-svn-id: http://svn.fortytwo.net/projects@325 4bf1a796-0a03-0410-9b82-c499c1e613c7

--- troika/phase2/trunk/api/src/parser/Ast.c
+++ troika/phase2/trunk/api/src/parser/Ast.c
@@ -18,7 +18,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA
 *******************************************************************************/
 
 #include <Ast.h>
-#include "settings.h"
+#include "../settings.h"
 
 
 /** If defined, print terms as raw integer sequences, rather than as
