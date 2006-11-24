86762760b5bf03cc75e3946aa705584646cc47cb
josh <josh@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2006-11-24 04:41:38 +0000

    git-svn-id: http://svn.fortytwo.net/projects@387 4bf1a796-0a03-0410-9b82-c499c1e613c7

--- troika/wurfel/src/main/java/wurfel/JLineTest.java
+++ troika/wurfel/src/main/java/wurfel/JLineTest.java
@@ -6,9 +6,13 @@ import org.openrdf.sesame.repository.local.LocalRepository;
 import org.openrdf.sesame.config.ConfigurationException;
 import org.openrdf.sesame.admin.AdminListener;
 import org.openrdf.sesame.admin.StdOutAdminListener;
-//import org.openrdf.rio.RDFFormat;
+import org.openrdf.sesame.constants.RDFFormat;
+import org.openrdf.sesame.config.AccessDeniedException;
 
 import java.net.URL;
+import java.net.MalformedURLException;
+
+import java.io.IOException;
 
 public class JLineTest
 {
@@ -26,15 +30,42 @@ public class JLineTest
         catch ( ConfigurationException e )
         {
             System.err.println( e.getMessage() );
+            return;
         }
-/*
-        URL myRDFData = new URL( "http://xmlns.com/foaf/0.1/index.rdf" );
+
+        URL myRDFData;
+
+        try
+        {
+            myRDFData = new URL( "http://xmlns.com/foaf/0.1/index.rdf" );
+        }
+
+        catch ( MalformedURLException e )
+        {
+            System.err.println( e.getMessage() );
+            return;
+        }
+
         String baseURI = "http://foaf#";
         boolean verifyData = true;
         AdminListener myListener = new StdOutAdminListener();
 
-        myRepository.addData( myRDFData, baseURI, RDFFormat.RDFXML, verifyData, myListener );
-*/
+        try
+        {
+            myRepository.addData( myRDFData, baseURI, RDFFormat.RDFXML, verifyData, myListener );
+        }
+
+        catch ( IOException e )
+        {
+            System.err.println( e.getMessage() );
+            return;
+        }
+
+        catch ( AccessDeniedException e )
+        {
+            System.err.println( e.getMessage() );
+            return;
+        }
     }
 
     public static void main( final String [] args )
