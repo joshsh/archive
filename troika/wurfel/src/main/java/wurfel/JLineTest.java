0f6633f691533f2396faa11fb0b03ae542f034bb
josh <josh@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2006-11-24 04:31:25 +0000

    Added a Sesame dependency.
    git-svn-id: http://svn.fortytwo.net/projects@386 4bf1a796-0a03-0410-9b82-c499c1e613c7

--- troika/wurfel/src/main/java/wurfel/JLineTest.java
+++ troika/wurfel/src/main/java/wurfel/JLineTest.java
@@ -1,8 +1,42 @@
 package wurfel;
 
+import org.openrdf.sesame.Sesame;
+import org.openrdf.sesame.repository.local.LocalService;
+import org.openrdf.sesame.repository.local.LocalRepository;
+import org.openrdf.sesame.config.ConfigurationException;
+import org.openrdf.sesame.admin.AdminListener;
+import org.openrdf.sesame.admin.StdOutAdminListener;
+//import org.openrdf.rio.RDFFormat;
+
+import java.net.URL;
 
 public class JLineTest
 {
+    private static void testSesame()
+    {
+        LocalService service = Sesame.getService();
+        boolean inferencing = true;
+        LocalRepository myRepository;
+
+        try
+        {
+             myRepository = service.createRepository( "myRep", inferencing );
+        }
+
+        catch ( ConfigurationException e )
+        {
+            System.err.println( e.getMessage() );
+        }
+/*
+        URL myRDFData = new URL( "http://xmlns.com/foaf/0.1/index.rdf" );
+        String baseURI = "http://foaf#";
+        boolean verifyData = true;
+        AdminListener myListener = new StdOutAdminListener();
+
+        myRepository.addData( myRDFData, baseURI, RDFFormat.RDFXML, verifyData, myListener );
+*/
+    }
+
     public static void main( final String [] args )
     {
 /*
@@ -15,6 +49,12 @@ public class JLineTest
             System.out.println( s );
         }
 */
+
+
+        testSesame();
+
+
+
 
         try
         {
