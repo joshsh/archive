597c0fc26d2258830ff9229fe37b4b25373825fd
josh <josh@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2007-02-27 02:10:01 +0000

    Beginnings of a stack processor.
    git-svn-id: http://svn.fortytwo.net/projects@523 4bf1a796-0a03-0410-9b82-c499c1e613c7

---
+++ troika/wurfel/src/main/java/ripple/Sink.java
@@ -0,0 +1,12 @@
+//package net.fortytwo.ripple.model;
+package ripple;
+
+//import net.fortytwo.ripple.RippleException;
+import wurfel.WurfelException;
+
+public interface Sink<T>
+{
+    public void put( T t ) throws WurfelException;
+}
+
+// kate: space-indent on; indent-width 4; tab-width 4; replace-tabs on
