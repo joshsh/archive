862390e760fbfd9f7b97ce6e3281b376f12de68c
josh <josh@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2007-05-04 00:05:14 +0000

    Implemented Joy's 'uncons' and 'unswons' operators.
    git-svn-id: http://svn.fortytwo.net/projects@680 4bf1a796-0a03-0410-9b82-c499c1e613c7

--- troika/ripple/trunk/src/main/java/net/fortytwo/ripple/extensions/joy/JoyExtension.java
+++ troika/ripple/trunk/src/main/java/net/fortytwo/ripple/extensions/joy/JoyExtension.java
@@ -47,7 +47,9 @@ public class JoyExtension implements Extension
 		bridge.add( new Time( new RdfValue( mc.createUri( ns + "time" ) ), mc ), mc );
 		bridge.add( new Times( new RdfValue( mc.createUri( ns + "times" ) ), mc ), mc );
 		bridge.add( new True( new RdfValue( mc.createUri( ns + "true" ) ), mc ), mc );
+		bridge.add( new Uncons( new RdfValue( mc.createUri( ns + "uncons" ) ), mc ), mc );
 		bridge.add( new Unstack( new RdfValue( mc.createUri( ns + "unstack" ) ), mc ), mc );
+		bridge.add( new Unswons( new RdfValue( mc.createUri( ns + "unswons" ) ), mc ), mc );
 		bridge.add( new Xor( new RdfValue( mc.createUri( ns + "xor" ) ), mc ), mc );
 	}
 }
