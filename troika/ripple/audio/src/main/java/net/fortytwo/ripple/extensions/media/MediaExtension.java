b78a76827eef7b44fee7762d5065e754827cc5d3
josh <josh@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2007-06-20 06:26:20 +0000

    Added a 'speak' primitive which speaks strings as text.
    git-svn-id: http://svn.fortytwo.net/projects@745 4bf1a796-0a03-0410-9b82-c499c1e613c7

---
+++ troika/ripple/audio/src/main/java/net/fortytwo/ripple/extensions/media/MediaExtension.java
@@ -0,0 +1,25 @@
+package net.fortytwo.ripple.extensions.media;
+
+import net.fortytwo.ripple.RippleException;
+import net.fortytwo.ripple.model.ModelBridge;
+import net.fortytwo.ripple.model.ModelConnection;
+import net.fortytwo.ripple.model.RdfValue;
+import net.fortytwo.ripple.util.Extension;
+import net.fortytwo.ripple.util.UrlFactory;
+
+public class MediaExtension implements Extension
+{
+	private static String ns = "http://fortytwo.net/2007/05/ripple/media#";
+
+	public void load( UrlFactory uf, ModelConnection mc )
+		throws RippleException
+	{
+		ModelBridge bridge = mc.getModel().getBridge();
+		uf.addMapping(
+			ns, getClass().getResource( "media.ttl" ) + "#" );
+
+		bridge.add( new Speak( new RdfValue( mc.createUri( ns + "speak" ) ), mc ), mc );
+	}
+}
+
+// kate: tab-width 4
---
+++ troika/ripple/audio/src/main/java/net/fortytwo/ripple/extensions/media/Speak.java
@@ -0,0 +1,73 @@
+package net.fortytwo.ripple.extensions.media;
+
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+import com.sun.speech.freetts.audio.JavaClipAudioPlayer;
+
+import net.fortytwo.ripple.RippleException;
+import net.fortytwo.ripple.model.ModelConnection;
+import net.fortytwo.ripple.model.PrimitiveFunction;
+import net.fortytwo.ripple.model.RdfValue;
+import net.fortytwo.ripple.model.RippleList;
+import net.fortytwo.ripple.model.RippleValue;
+import net.fortytwo.ripple.util.Sink;
+
+public class Speak extends PrimitiveFunction
+{
+	public Speak( RdfValue v, ModelConnection mc )
+		throws RippleException
+	{
+		super( v, mc );
+
+		if ( !initialized )
+			init();
+	}
+
+	public int arity()
+	{
+		return 1;
+	}
+
+	public void applyTo( RippleList stack,
+						Sink<RippleList> sink,
+						ModelConnection mc )
+		throws RippleException
+	{
+		String s;
+
+		s = mc.stringValue( stack.getFirst() );
+		stack = stack.getRest();
+
+		singleVoice.speak( s );
+	}
+
+	static Voice singleVoice;
+
+	static boolean initialized = false;
+
+	static void init()
+		throws RippleException
+	{
+		String voiceName = "kevin";
+
+		VoiceManager voiceManager = VoiceManager.getInstance();
+		singleVoice = voiceManager.getVoice( voiceName );
+
+		if ( null == singleVoice )
+			throw new RippleException(
+				"Cannot find a voice named " + voiceName );
+		
+		singleVoice.allocate();
+
+		initialized = true;
+	}
+
+	// Note: never called.
+	static void shutdown()
+	{
+		if ( initialized )
+			singleVoice.deallocate();
+	}
+}
+
+// kate: tab-width 4
