069dd85b949c782bb9f86842321f68025dc8d772
josh <josh@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2007-06-28 19:47:59 +0000

    Got rid of the unnecessary 'filter' package.
    git-svn-id: http://svn.fortytwo.net/projects@759 4bf1a796-0a03-0410-9b82-c499c1e613c7

--- troika/ripple/trunk/src/main/java/net/fortytwo/ripple/model/Op.java
+++ troika/ripple/trunk/src/main/java/net/fortytwo/ripple/model/Op.java
@@ -1,10 +1,10 @@
 package net.fortytwo.ripple.model;
 
 import net.fortytwo.ripple.RippleException;
-import net.fortytwo.ripple.model.filter.Filter;
+import net.fortytwo.ripple.io.RipplePrintStream;
 import net.fortytwo.ripple.util.Sink;
 
-public class Op extends Filter
+public class Op implements Function, RippleValue
 {
 	public void applyTo( RippleList stack,
 				Sink<RippleList> sink,
@@ -27,6 +27,33 @@ public class Op extends Filter
 			return 0;
 		else
 			return this.getClass().getName().compareTo( other.getClass().getName() );
+	}
+
+	public int arity()
+	{
+		return 1;
+	}
+
+	public void printTo( RipplePrintStream p )
+		throws RippleException
+	{
+		System.err.println( "You should not need to print op directly." );
+		System.exit( 1 );
+	}
+
+	public RdfValue toRdf( ModelConnection mc )
+		throws RippleException
+	{
+		System.err.println( "You should not need to convert op explicitly." );
+		System.exit( 1 );
+		return null;
+	}
+
+	public boolean isActive()
+	{
+		System.err.println( "op is always active, but your algorithm should know that without calling isActive()." );
+		System.exit( 1 );
+		return true;
 	}
 }
 
