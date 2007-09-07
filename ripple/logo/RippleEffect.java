609cf6575bbd75a258274db29cff5198181c9487
josh <josh@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2007-09-07 22:09:41 +0000

    Tweaked (over a week ago) the logo generator to produce a blog header image for ripple.fortytwo.net.
    git-svn-id: http://svn.fortytwo.net/projects@862 4bf1a796-0a03-0410-9b82-c499c1e613c7

--- ripple/logo/RippleEffect.java
+++ ripple/logo/RippleEffect.java
@@ -21,7 +21,7 @@ public class RippleEffect
 	Point2D mid;
 	Point2D[] centers;
 	double radius, rotateBy, frequencyFactor;
-	double radiusRest, tanFactor;
+	double rippleRadius, dampenRadius, radiusRest, tanFactor;
 
 	Color[] colors;
 	Color backgroundColor;
@@ -85,7 +85,7 @@ System.out.println( "new slosh: " + magnitude + " at " + location );
 
 	double dampen( double distance )
 	{
-		return Math.cos( ( distance / radiusRest ) * Math.PI / 2.0 );
+		return Math.cos( ( distance / dampenRadius ) * Math.PI / 2.0 );
 	}
 
 	double dampen( Point2D p )
@@ -103,17 +103,30 @@ System.out.println( "new slosh: " + magnitude + " at " + location );
 
 	double waveFunc( double distance )
 	{
-		if ( distance >= radiusRest )
+		if ( distance >= rippleRadius )
 			return 0;
 		else
 			return
 				dampen( distance ) *
-				Math.cos(
+//				Math.cos(
+Math.sin(
 					frequencyFactor * Math.tan( distance * tanFactor ) );
 //				dampen( distance ) *
 //				( 1.0 + Math.cos(
 //					frequencyFactor * Math.tan( distance * tanFactor ) ) ) / 2.0;
 //				* ( 1.0 + Math.cos( frequencyFactor / ( radiusRest - distance ) ) ) / 2.0;
+	}
+
+	// Just a debug method.
+	void drawCenters()
+	{
+		for ( int i = 0; i < 3; i++ )
+		{
+			Point2D p = centers[i];
+			int x = (int) ( width * p.getX() );
+			int y = (int) ( width * p.getY() );
+			matrix[y][x] = Color.RED;
+		}
 	}
 
 	// Just a debug method.
@@ -145,9 +158,6 @@ System.out.println( "new slosh: " + magnitude + " at " + location );
 
 	void createPoints()
 	{
-		radiusRest = 0.5 - radius;
-		tanFactor = Math.PI / ( radiusRest * 2 );
-
 		mid = new Point2D.Double( 0.5, 0.5 );
 		centers = new Point2D.Double[3];
 
@@ -160,7 +170,7 @@ System.out.println( "new slosh: " + magnitude + " at " + location );
 		}
 	}
 
-	void createField()
+	void createField_WithColors()
 		throws Exception
 	{
 		matrix = new Color[height][width];
@@ -351,6 +361,106 @@ for ( int k = 0; k < nBrighten; k++ )
 		}
 	}
 
+
+
+
+
+	void createField()
+		throws Exception
+	{
+		matrix = new Color[height][width];
+
+		for ( int i = 0; i < height; i++ )
+		{
+			for ( int j = 0; j < width; j++ )
+			{
+				Point2D p = new Point2D.Double(
+					(double) i / (double) height,
+					1.0 - ( (double) j / (double) width ) );
+
+				if ( p.distance( mid ) >= 0.5 )
+				{
+					matrix[i][j] = backgroundColor;
+					continue;
+				}
+
+				double dist0 = p.distance( centers[0] );
+				double dist1 = p.distance( centers[1] );
+				double dist2 = p.distance( centers[2] );
+
+				// Don't draw anything beyond the trefoil border.
+				if ( dist0 >= radiusRest && dist1 >= radiusRest && dist2 >= radiusRest )
+				{
+					matrix[i][j] = backgroundColor;
+					continue;
+				}
+
+				double val0 = waveFunc( dist0 );
+				double val1 = waveFunc( dist1 );
+				double val2 = waveFunc( dist2 );
+
+				double slosh = sloshField[i][j];
+
+				double intensity = ( val0 + val1 + val2 + ( slosh * sloshWeight ) ) / ( 3.0 + sloshWeight );
+
+
+//				double intensity = ( val0 + val1 + val2 ) / 3.0;
+//				double intensity = 1;
+//				double intensity = slosh / sloshWeight;
+
+				// Fade out at edges of trefoil.
+				intensity = intensity * dampen( p );
+
+				if ( intensity < 0 )
+					intensity = 0;
+
+intensity = intensify( intensity, 4 );
+//*
+int v = (int) ( 255 * intensity );
+Color result = new Color( 255, 255, 255, v );
+//*/
+
+/*
+				if ( val0 < 0 )
+					val0 = 0;
+				if ( val1 < 0 )
+					val1 = 0;
+				if ( val2 < 0 )
+					val2 = 0;
+
+				double m0_a = val0;
+				double m1_a = val1;
+				double m2_a = val2;
+
+				double m0_b = dist0 > radiusRest ? 0 : 1.0 - ( dist0 / radiusRest );
+				double m1_b = dist1 > radiusRest ? 0 : 1.0 - ( dist1 / radiusRest );
+				double m2_b = dist2 > radiusRest ? 0 : 1.0 - ( dist2 / radiusRest );
+
+double r = m0_b;
+double g = m1_b;
+double b = m2_b;
+
+
+Color med = new Color( (float) r, (float) g, (float) b );
+//int nBrighten = 5;
+int nBrighten = 3;
+for ( int k = 0; k < nBrighten; k++ )
+	med = med.brighter();
+
+				Color result = new Color( med.getRed(), med.getGreen(), med.getBlue(), (int) (255*intensity) );
+//				Color result = new Color( (float) r, (float) g, (float) b, (float) intensity ).brighter();
+//*/
+				matrix[i][j] = result;
+			}
+		}
+	}
+
+
+
+
+
+
+
 	void createSloshes()
 	{
 		sloshConstB = 2 * sloshConstA / Math.PI;
@@ -418,17 +528,39 @@ System.out.println( "max = " + max );
 	void init()
 		throws Exception
 	{
-		radius = 0.15;
-		rotateBy = 1/2.0 + 1/20.0;
-		frequencyFactor = 9.0;
-		width = 400;
-		height = 400;
+		boolean wide = true;
+
+		if ( wide )
+		{
+			radius = 0.06;
+			rotateBy = 1/2.0 + 1/20.0;
+			frequencyFactor = 20.0;
+			backgroundColor = Color.BLUE;
+		}
+
+		else
+		{
+			radius = 0.15;
+			rotateBy = 1/2.0 + 1/20.0;
+			frequencyFactor = 9.0;
+			backgroundColor = Color.WHITE;
+		}
+
+		radiusRest = 0.5 - radius;
+		dampenRadius = radiusRest;
+//		rippleRadius = radiusRest;
+rippleRadius = radiusRest * 1.2;
+		tanFactor = Math.PI / ( rippleRadius * 2 );
+
+		width = 1000;
+		height = 1000;
 
 		nSloshes = 40;
 		sloshWeight = 2.0;
 		sloshConstA = 1.0;
 		sloshPointiness = 20.0;
-		randomSeed = 2750;
+randomSeed = 8212;
+//		randomSeed = 2750;
 //		randomSeed = ( new Random() ).nextInt( 10000 );
 
 		colors = new Color[3];
@@ -436,14 +568,14 @@ System.out.println( "max = " + max );
 		colors[1] = Color.GREEN;
 		colors[2] = Color.BLUE;
 
-		backgroundColor = Color.WHITE;
 
 		random = new Random( randomSeed );
 
 		createPoints();
 		createSloshes();
 		createField();
-drawSloshCenters();
+//drawSloshCenters();
+//drawCenters();
 		createImage();
 
 		System.out.println( "randomSeed = " + randomSeed );
