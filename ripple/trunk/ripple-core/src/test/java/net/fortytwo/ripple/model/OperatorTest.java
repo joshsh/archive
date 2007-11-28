397afff3614693ef3b7ca0af2f7a9d8c1ec8e607
josh <josh@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2007-11-28 05:03:56 +0000

    Added a unit test for operator dispatching behavior.
    git-svn-id: http://svn.fortytwo.net/projects@930 4bf1a796-0a03-0410-9b82-c499c1e613c7

---
+++ ripple/trunk/ripple-core/src/test/java/net/fortytwo/ripple/model/OperatorTest.java
@@ -0,0 +1,129 @@
+package net.fortytwo.ripple.model;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.rio.RDFFormat;
+
+import net.fortytwo.ripple.io.RdfImporter;
+import net.fortytwo.ripple.libs.stack.Dup;
+import net.fortytwo.ripple.query.ListDequotation;
+import net.fortytwo.ripple.rdf.RdfUtils;
+import net.fortytwo.ripple.rdf.SesameInputAdapter;
+import net.fortytwo.ripple.test.RippleTestCase;
+import net.fortytwo.ripple.util.Collector;
+
+public class OperatorTest extends RippleTestCase
+{
+	private static final String TEST_1 =
+  "@prefix : <urn:test.CreateOperatorTest#>.\n"
++ "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\n"
++ "\n"
++ ":simpleList a rdf:List;\n"
++ "    rdf:first \"1\";\n"
++ "    rdf:rest [\n"
++ "        rdf:first \"2\";\n"
++ "        rdf:rest rdf:nil ];\n"
++ "    .\n"
++ ":firstBranchingList a rdf:List;\n"
++ "rdf:first \"1a\";\n"
++ "rdf:first \"1b\";\n"
++ "rdf:rest [\n"
++ "		rdf:first \"2\";\n"
++ "		rdf:rest rdf:nil ];\n"
++ ".";
+	
+	private class CreateOperatorTest extends TestRunnable
+	{
+		public void test()
+			throws Exception
+		{
+			Model model = getTestModel();
+			ModelConnection mc = model.getConnection( "for CreateOperatorTest" );
+			
+			InputStream is = new ByteArrayInputStream( TEST_1.getBytes() );
+			RdfImporter importer = new RdfImporter( mc );
+			SesameInputAdapter sc = new SesameInputAdapter( importer );
+			RdfUtils.read( is, sc, "", RDFFormat.TURTLE );
+			mc.commit();
+			is.close();
+			
+			Collector<Operator> ops = new Collector<Operator>();
+			RippleValue arg;
+			RippleValue a1 = new RdfValue( mc.createLiteral( "1" ) );
+			RippleValue a2 = new RdfValue( mc.createLiteral( "2" ) );
+			
+			// a RippleList --> ListDequotation
+			arg = new RippleList( a2 ).push( a1 );
+			ops.clear();
+			Operator.createOperator( arg, ops, mc );
+			assertEquals( 1, ops.size() );
+			assertTrue( ops.iterator().next().getFunction() instanceof ListDequotation );
+			
+			// the nil RippleList --> ListDequotation
+			arg = RippleList.NIL;
+			ops.clear();
+			Operator.createOperator( arg, ops, mc );
+			assertEquals( 1, ops.size() );
+			assertTrue( ops.iterator().next().getFunction() instanceof ListDequotation );
+			
+			// rdf:nil --> ListDequotation
+			arg = new RdfValue( RDF.NIL );
+			ops.clear();
+			Operator.createOperator( arg, ops, mc );
+			assertEquals( 1, ops.size() );
+			assertTrue( ops.iterator().next().getFunction() instanceof ListDequotation );
+			
+			// an rdf:List --> ListDequotation
+			arg = new RdfValue( mc.createUri( "urn:test.CreateOperatorTest#simpleList" ) );
+			ops.clear();
+			Operator.createOperator( arg, ops, mc );
+			assertEquals( 1, ops.size() );
+			assertTrue( ops.iterator().next().getFunction() instanceof ListDequotation );
+			
+			// a branching rdf:List --> multiple ListDequotations
+			arg = new RdfValue( mc.createUri( "urn:test.CreateOperatorTest#firstBranchingList" ) );
+			ops.clear();
+			Operator.createOperator( arg, ops, mc );
+			assertEquals( 2, ops.size() );
+			assertTrue( ops.iterator().next().getFunction() instanceof ListDequotation );
+			
+			// a PrimitiveFunction --> the same PrimitiveFunction
+			arg = new Dup();
+			ops.clear();
+			Operator.createOperator( arg, ops, mc );
+			assertEquals( 1, ops.size() );
+			assertTrue( ops.iterator().next().getFunction() instanceof Dup );
+			
+			// an rdf:Property --> RdfPredicateFunction
+			arg = new RdfValue( RDF.TYPE );
+			ops.clear();
+			Operator.createOperator( arg, ops, mc );
+			assertEquals( 1, ops.size() );
+			assertTrue( ops.iterator().next().getFunction() instanceof RdfPredicateFunction );
+
+			// a non-property RdfValue which is not anything else --> RdfPredicateFunction
+			arg = new RdfValue( RDF.BAG );
+			ops.clear();
+			Operator.createOperator( arg, ops, mc );
+			assertEquals( 1, ops.size() );
+			assertTrue( ops.iterator().next().getFunction() instanceof RdfPredicateFunction );
+			
+			// anything else --> NullFilter
+			arg = new NumericLiteral( 42 );
+			ops.clear();
+			Operator.createOperator( arg, ops, mc );
+			assertEquals( 1, ops.size() );
+			assertTrue( ops.iterator().next().getFunction() instanceof NullFilter );
+			
+			mc.close();
+		}
+	}
+
+	public void runTests()
+		throws Exception
+	{
+		testAsynchronous( new CreateOperatorTest() );
+	}
+}
