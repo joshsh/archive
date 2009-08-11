d3d947381f056480d65d152b95f9d4248349d4ac
josh <josh@4bf1a796-0a03-0410-9b82-c499c1e613c7>
2009-08-11 21:26:29 +0000

    Updgraded to Restlet 1.1.5
    git-svn-id: http://svn.fortytwo.net/projects@1105 4bf1a796-0a03-0410-9b82-c499c1e613c7

--- rdfwiki/trunk/src/main/java/net/fortytwo/rdfwiki/RDFWiki.java
+++ rdfwiki/trunk/src/main/java/net/fortytwo/rdfwiki/RDFWiki.java
@@ -15,47 +15,47 @@ import org.restlet.Context;
 import org.restlet.data.Protocol;
 import org.restlet.data.Request;
 
-public class RDFWiki
-{
-	public static final String WIKI_ATTR = "wiki";
+public class RDFWiki {
+    public static final String WIKI_ATTR = "wiki";
 
-	private SailSelector sailSelector;
-	
-// FIXME: temporary
-private static RDFWiki singleWiki;
+    private SailSelector sailSelector;
 
-	public static RDFWiki getWiki( final Context context )
-	{
+    // FIXME: temporary
+    private static RDFWiki singleWiki;
+
+    public static RDFWiki getWiki(final Context context) {
 //		return (RdfWiki) context.getAttributes().get( WIKI_ATTR );
-		return singleWiki;
-	}
-	
-	public Sail getSail( final Request request ) throws Exception
-	{
-		return sailSelector.selectSail( request );
-	}
-		
-	public RDFWiki( final SailSelector selector ) throws Exception
-	{
-singleWiki = this;
+        return singleWiki;
+    }
 
-		sailSelector = selector;
-		
-		// Create a new Restlet component and add a HTTP server connector to it
-		Component component = new Component();
-		component.getServers().add( Protocol.HTTP, 8182 );
-		
+    public Sail getSail(final Request request) throws Exception {
+        return sailSelector.selectSail(request);
+    }
+
+    public RDFWiki(final SailSelector selector) throws RDFWikiException {
+        singleWiki = this;
+
+        sailSelector = selector;
+
+        // Create a new Restlet component and add a HTTP server connector to it
+        Component component = new Component();
+        component.getServers().add(Protocol.HTTP, 8182);
+
 //		Restlet resourceRestlet = new ResourceRestlet();
 //		component.getDefaultHost().attach( "/resource/", resourceRestlet );
-		
+
 //		Restlet htmlRestlet = new HtmlRestlet();
 //		component.getDefaultHost().attach( "/page/", htmlRestlet );
 
 //		Restlet sparqlRestlet = new SparqlRestlet();
 //		component.getDefaultHost().attach( "/sparql", sparqlRestlet );
 
-		component.getDefaultHost().getContext().getAttributes().put( WIKI_ATTR, this );
-		component.getDefaultHost().attach( new RootApplication() );
-		component.start();
-	}
+        component.getDefaultHost().getContext().getAttributes().put(WIKI_ATTR, this);
+        component.getDefaultHost().attach(new RootApplication());
+        try {
+            component.start();
+        } catch (Exception e) {
+            throw new RDFWikiException(e);
+        }
+    }
 }
---
+++ rdfwiki/trunk/src/main/java/net/fortytwo/rdfwiki/RDFWikiException.java
@@ -0,0 +1,14 @@
+package net.fortytwo.rdfwiki;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: josh
+ * Date: Aug 11, 2009
+ * Time: 12:30:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RDFWikiException extends Exception {
+    public RDFWikiException(final Throwable cause) {
+        super(cause);
+    }
+}
